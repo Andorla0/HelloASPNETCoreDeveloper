8a70c9e8539a31ab64bd75588a06249526dd9a36
andorla <andrebp1020@gmail.com>
2024-08-23 17:57:57 -0500

    feature(welcome):  added ewlcome endpoint.

---
+++ HelloASPNETCoreDeveloper/Generic/Interfaces/REST/Controllers/WelcomeController.cs
@@ -0,0 +1,22 @@
+using HelloASPNETCoreDeveloper.Generic.Domain.Model.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPNETCoreDeveloper.Generic.Interfaces.REST.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WelcomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Welcome()
+        {
+            return WelcomeBuilder.Build(null);
+        }
+
+        [HttpGet("{name}")]
+        public string welcome(string name)
+        {
+            return WelcomeBuilder.Build(name);
+        }
+    }
+}
